74ded08428f7a014fcc51ab50585a74c37ebec88
Yarden Mathieson <91827086+YardenM2710@users.noreply.github.com>
2022-03-01 15:09:42 +0200

    Delete config directory

--- config/prod.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  // dbURL: 'mongodb://localhost:27017',
-  dbURL:
-    'mongodb+srv://Yarden:TYXFlHHRgdH1U7hO@cluster0.lsmqg.mongodb.net/toy_db?retryWrites=true&w=majority',
-};
